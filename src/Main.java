cf738cc09058006c4df0ed27d0bf427064cd0cb7
Evgeny Boltunov <e.boltunov@syncleo-iot.com>
2020-03-31 00:20:31 +0300

    Create main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int CurrentBalance = 2_000_000_000;
+        int MoneyTransfer = 500_000_000;
+        int EndBalance = CurrentBalance * MoneyTransfer;
+        System.out.println(EndBalance);
+    }
+}
