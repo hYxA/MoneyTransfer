886bd368378340072e7279cae341047b82d4c9aa
Evgeny Boltunov <e.boltunov@syncleo-iot.com>
2020-03-31 00:33:45 +0300

    Change math action

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int CurrentBalance = 2_000_000_000;
         int MoneyTransfer = 500_000_000;
-        int EndBalance = CurrentBalance * MoneyTransfer;
-        System.out.println(EndBalance);
+        int EndBalance = CurrentBalance + MoneyTransfer;
+        System.out.println("EndBalance =" + EndBalance);
     }
 }
