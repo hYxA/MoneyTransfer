e657b2859915b29524ef4216290e5905f2d5bb01
Igor Volnukhin <zybu@live.ru>
2020-04-04 12:18:40 +0300

    Correct mistakes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long CurrentBalance = 2_000_000_000;
-        long MoneyTransfer = 500_000_000;
-        long EndBalance = CurrentBalance + MoneyTransfer;
+        int CurrentBalance = 2_000_000_000;
+        int MoneyTransfer = 500_000_000;
+        int EndBalance = CurrentBalance + MoneyTransfer;
         System.out.println("EndBalance =" + EndBalance);
     }
 }
